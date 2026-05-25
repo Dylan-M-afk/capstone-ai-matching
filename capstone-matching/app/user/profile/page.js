20df0db6da3fcb8c7be7434fdc18535859dd42e2
Forward <Julia.Forward@k202domain.com>
2026-05-25 13:17:42 -0230

    feat: Added student profile UI skeleton; Added several fields; Added save button and non-functional progress bar;

---
+++ capstone-matching/app/user/profile/page.js
@@ -0,0 +1,95 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="profile-page-container ">
+
+      <p className="profile-header-text">Student Profile</p>
+
+      <div className="profile-content-container">
+
+        <div className="profile-content-lr">
+
+          <div className="profile-content-left">
+
+            <div className="profile-form-row ">
+              <label className="profile-form-label">
+                Full Name:
+              </label>
+              <input
+                className="profile-form-field"
+                id="fullname"
+                type="text"
+                placeholder="Jane Doe">
+              </input>
+            </div>
+
+            <div className="profile-form-row ">
+              <label className="profile-form-label">
+                Program:
+              </label>
+              <input
+                className="profile-form-field"
+                id="program"
+                type="text"
+                placeholder="Bachelors of Engineering">
+              </input>
+            </div>
+
+            <div className="profile-form-row ">
+              <label className="profile-form-label">
+                Skills (comma seperated):
+              </label>
+              <input
+                className="profile-form-field"
+                id="skills"
+                type="text"
+                placeholder="Java, HTML, Python, ...">
+              </input>
+            </div>
+
+            <div className="profile-form-row ">
+              <label className="profile-form-label">
+                Skills (comma seperated):
+              </label>
+              <input
+                className="profile-form-field"
+                id="skills"
+                type="text"
+                placeholder="Java, HTML, Python, ...">
+              </input>
+            </div>
+
+            <div className="profile-form-row ">
+              <label className="profile-form-label">
+                Bio:
+              </label>
+              <textarea
+                className="profile-form-field h-20"
+                id="skills"
+                type="text"
+                placeholder="I am an experienced junior backend-engineer with specialisation for the spring framework...">
+              </textarea>
+            </div>
+
+          </div>
+
+          <div className="profile-content-right">
+            test
+          </div>
+
+        </div>
+
+        <div className="profile-content-bottom">
+          <div className="progress-bar-container">
+            <div className="progress-bar-value"></div>
+          </div>
+
+          <button className="button">Save Changes</button>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
