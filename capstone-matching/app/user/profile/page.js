67936d7380b70ab62911c0fe1815d796dac28c33
UnusualFrog <forwardnoah@gmail.com>
2026-05-26 20:10:35 -0230

    feat: Added form validation with error msg

--- capstone-matching/app/user/profile/page.js
+++ capstone-matching/app/user/profile/page.js
@@ -4,31 +4,65 @@ import { useState } from 'react'
 
 export default function Home() {
 
+  // Field Values
+  const [fullname, setFullName] = useState('');
+  const [program, setProgram] = useState('');
+  const [skills, setSkills] = useState('');
+  const [availability, setAvailability] = useState('');
+  const [bio, setBio] = useState('');
+
+  // Experience List logic
   const [expName, setExpName] = useState('');
   const [expDate, setExpDate] = useState('');
   const [expItems, setExpItems] = useState([])
+
+  // Validation
+  const [error, setError] = useState('');
 
 
   function addExpItem() {
     console.log("adding item")
 
-    // console.log(...expItems)
     setExpItems(expItems => [...expItems, {
       "name": expName,
       "years": expDate
     }])
-    console.log(expItems);
-
-    // setExpName('');
-    // setExpDate('');
   }
 
   function deleteExpItem(i) {
-    console.log(expItems)
+    console.log("deleting item at index" + i)
+
     setExpItems(expItems => expItems.filter(
       (_, idx) => i !== idx
     ))
-    
+
+  }
+
+  function validate() {
+    if (fullname == '') {
+      setError('Full Name must not be blank');
+      return false;
+    }
+    if (program == '') {
+      setError('Program must not be blank');
+      return false;
+    }
+    if (skills == '') {
+      setError('Skills must not be blank');
+      return false;
+    }
+    if (availability == '') {
+      setError('Availability must not be blank');
+      return false;
+    }
+    if (bio == '') {
+      setError('Bio must not be blank');
+      return false;
+    }
+
+
+    setError('')
+    return true;
   }
 
   return (
@@ -37,7 +71,7 @@ export default function Home() {
       {/* Header */}
       <p className="profile-header-text">Student Profile</p>
 
-      <form className="profile-content-container">
+      <form action="ADD ME" onSubmit={(e) => { if (!validate()) e.preventDefault() }} className="profile-content-container">
 
         <div className="profile-content-lr">
 
@@ -53,7 +87,9 @@ export default function Home() {
                 id="fullname"
                 name="fullname"
                 type="text"
-                placeholder="Jane Doe">
+                placeholder="Jane Doe"
+                onChange={e => setFullName(e.target.value)}
+              >
               </input>
             </div>
 
@@ -66,7 +102,9 @@ export default function Home() {
                 id="program"
                 name="program"
                 type="text"
-                placeholder="Bachelors of Engineering">
+                placeholder="Bachelors of Engineering"
+                onChange={e => setProgram(e.target.value)}
+              >
               </input>
             </div>
 
@@ -79,7 +117,9 @@ export default function Home() {
                 id="skills"
                 name="skills"
                 type="text"
-                placeholder="Java, HTML, Python, ...">
+                placeholder="Java, HTML, Python, ..."
+                onChange={e => setSkills(e.target.value)}
+              >
               </input>
             </div>
 
@@ -92,7 +132,9 @@ export default function Home() {
                 id="skills"
                 name="availability"
                 type="text"
-                placeholder="Mon-Fri 9AM-5PM">
+                placeholder="Mon-Fri 9AM-5PM"
+                onChange={e => setAvailability(e.target.value)}
+              >
               </input>
             </div>
 
@@ -105,7 +147,9 @@ export default function Home() {
                 id="skills"
                 name="bio"
                 type="text"
-                placeholder="I am an experienced junior backend-engineer with specialisation for the spring framework...">
+                placeholder="I am an experienced junior backend-engineer with specialisation for the spring framework..."
+                onChange={e => setBio(e.target.value)}
+              >
               </textarea>
             </div>
 
@@ -133,7 +177,7 @@ export default function Home() {
                   type="text"
                   placeholder="Generic Company LLC."
                   onChange={(e) => setExpName(e.target.value)}
-                  >
+                >
                 </input>
                 <input
                   className="profile-experience-date-field "
@@ -141,15 +185,15 @@ export default function Home() {
                   type="number"
                   placeholder="2"
                   onChange={(e) => setExpDate(e.target.value)}
-                  >
+                >
                 </input>
               </div>
-              <button 
-                className="profile-add-experience-button" 
+              <button
+                className="profile-add-experience-button"
                 type="button"
                 onClick={addExpItem}
               >
-                  Add Experience
+                Add Experience
               </button>
 
               {/* Experience List */}
@@ -166,11 +210,11 @@ export default function Home() {
                       </div>
                       <button
                         id={i}
-                        className="experience-item-delete" 
+                        className="experience-item-delete"
                         type="button"
                         onClick={() => deleteExpItem(i)}
                       >
-                          X
+                        X
                       </button>
                     </div>
 
@@ -194,6 +238,11 @@ export default function Home() {
           <div className="progress-bar-container">
             <div className="progress-bar-value"></div>
           </div>
+
+          {error != '' && 
+            <div className='profile-form-error-container'>
+              <p className='profile-form-error-text'>Error: {error}</p>
+            </div>}
 
           {/* Submit Button */}
           <button className="button" type="submit">Save Changes</button>
