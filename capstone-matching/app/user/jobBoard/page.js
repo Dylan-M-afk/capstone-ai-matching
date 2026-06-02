9064c36492f340b6cdb544798155b59aa0bd9319
UnusualFrog <forwardnoah@gmail.com>
2026-06-02 19:29:10 -0230

    feat: dynamic generation of job items

--- capstone-matching/app/user/jobBoard/page.js
+++ capstone-matching/app/user/jobBoard/page.js
@@ -1,9 +1,41 @@
 
 
 export default function Home() {
-  return (
-    <div>
-      <p>Hello World! Welcome to User Dashboard</p>
-    </div>
-  );
+    let dummyData = [
+        {
+            "title": "Fish Handler",
+            "description": "Handle fish very very well"
+        },
+        {
+            "title": "Fish Thrower",
+            "description": "Throw fish at the wall fish very very well"
+        },
+        {
+            "title": "Fish Feeder",
+            "description": "Feed fish car batteries fish at the wall fish very very well"
+        }
+    ]
+
+    return (
+        <div className="sjb-page-container">
+            {/* Header */}
+            <p className="sjb-header-text ">Student Job Board</p>
+
+            {/* List of Job Items */}
+            <div className="sjb-job-list-container">
+                {
+                    dummyData.map((item, idx) => (
+                        <div className="sjb-job-item-container" key={idx}>
+                            <div className="sbj-job-item-top-container">
+
+                            </div>
+                            <div className="sbj-job-item-bottom-container">
+
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
 }
