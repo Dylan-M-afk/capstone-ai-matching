1ca3b2203d06b4d545f8077b42b181cac5a9b768
UnusualFrog <forwardnoah@gmail.com>
2026-06-03 23:16:43 -0230

    feat: job board allows students to apply to jobs, creating a row in the application table; validation for double-application and non-open status jobs

--- capstone-matching/app/user/jobBoard/page.js
+++ capstone-matching/app/user/jobBoard/page.js
@@ -5,7 +5,8 @@ import { createClient } from '@/utils/supabase/client'
 const supabase = createClient()
 
 export default function Home() {
-    const [jobs, setJobs] = useState([])
+    const [jobs, setJobs] = useState([]);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         async function fetchJobs() {
@@ -14,7 +15,10 @@ export default function Home() {
                 .select(`
                         *,
                         companies!job_posts_company_id_fkey (
-                        company_name
+                            company_name
+                        ),
+                        applications (
+                            status
                         )
                     `)
 
@@ -24,7 +28,30 @@ export default function Home() {
 
         fetchJobs()
     }, [])
-    console.log(jobs)
+    // console.log(jobs)
+
+    async function ApplyToJob(e) {
+        // if (!validate()) return
+        console.log("Applying to job: ")
+        console.log(e.target.id)
+        const { data: { user } } = await supabase.auth.getUser()
+
+        const { error: insertError } = await supabase
+            .from('applications')
+            .insert([{
+                student_id: user.id,
+                job_id: e.target.id,
+                status: "Submitted"
+            }])
+
+        if (insertError) {
+            console.error(insertError)
+            setError('Failed to apply to job posting: ' + e.target.id)
+            return
+        }
+
+        alert('Successfully Applied!')
+    }
 
     return (
         <div className="sjb-page-container">
@@ -36,33 +63,40 @@ export default function Home() {
                 {
                     jobs.map((item, idx) => (
                         <div className="sjb-job-item-container" key={idx}>
-                            <div className="sbj-job-item-top-container">
-                                <div className="sbj-job-item-top-left">
-                                    <p className="sbj-job-item-title-text">{item.title}</p>
-                                    <p className="sbj-job-item-company-text">
+                            <div className="sjb-job-item-top-container">
+                                <div className="sjb-job-item-top-left">
+                                    <p className="sjb-job-item-title-text">{item.title}</p>
+                                    <p className="sjb-job-item-company-text">
                                         {item.companies?.company_name}
-                                        <span className="sbj-job-item-info-text">Type: {item?.job_type} | Location: {item?.location} | Status: {item?.status}</span>
+                                        <span className="sjb-job-item-info-text">
+                                            Type: {item?.job_type} | Location: {item?.location} | Status: {item?.status}
+                                        </span>
                                     </p>
                                 </div>
-                                <div className="sbj-job-item-top-right">
-                                    <button className="button m-5 text-2xl">Apply</button>
+                                <div className="sjb-job-item-top-right">
+                                    <button id={item.id}
+                                            onClick={ApplyToJob} 
+                                            disabled={(item?.applications[0]?.status == undefined) && (item?.status == "Open") ? false : true} 
+                                            className={(item?.applications[0]?.status == undefined) && (item?.status == "Open") ? "sjb-apply-btn" : "sjb-apply-disabled"}>
+                                        {item?.applications[0]?.status == undefined ? "Apply" : "Applied"}
+                                    </button>
                                 </div>
                             </div>
-                            <div className="sbj-job-item-bottom-container">
-                                <div className="sbj-job-item-bottom-about-container">
+                            <div className="sjb-job-item-bottom-container">
+                                <div className="sjb-job-item-bottom-about-container">
                                     <p className="font-bold">About: </p>
                                     <p>{item?.description}</p>
                                 </div>
 
-                                <div className="sbj-job-item-bottom-skills-container"><p className="font-bold">Required Skills:</p>
-                                    <ul className="sbj-job-item-skills-list">
+                                <div className="sjb-job-item-bottom-skills-container"><p className="font-bold">Required Skills:</p>
+                                    <ul className="sjb-job-item-skills-list">
                                         {item?.required_skills?.map((item, idx) => (
-                                            <li className="sbj-job-item-skills-list-item" key={idx}>{item}</li>
+                                            <li className="sjb-job-item-skills-list-item" key={idx}>{item}</li>
                                         ))}
                                     </ul>
                                 </div>
 
-                                <div className="sbj-job-item-bottom-schedule-container">
+                                <div className="sjb-job-item-bottom-schedule-container">
                                     <p className="font-bold">Schedule:</p>
                                     <div>
                                         {item?.schedule?.split(" | ").map((item, idx) => (
