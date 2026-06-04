3a372bc325eda4cf5e038fccc8ceee27ce06fe39
UnusualFrog <forwardnoah@gmail.com>
2026-06-03 21:44:36 -0230

    feat: student job board grabs job posts from DB and populates fields with data; improved styling of UI

--- capstone-matching/app/user/jobBoard/page.js
+++ capstone-matching/app/user/jobBoard/page.js
@@ -1,35 +1,30 @@
+"use client"
 
+import { useEffect, useState } from 'react'
+import { createClient } from '@/utils/supabase/client'
+const supabase = createClient()
 
 export default function Home() {
-    let dummyData = [
-        {
-            "title": "Fish Handler",
-            "description": "Handle fish very very very very very very very very very very very very very very very very very very well",
-            "type": "Finance",
-            "location": "Remote",
-            "status": "Applied",
-            "skills": ["HTML", "CSS", "Java"],
-            "schedule": "9AM-5PM Mon-Fri"
-        },
-        {
-            "title": "Fish Thrower",
-            "description": "Throw fish at the wall fish very very well",
-            "type": "IT",
-            "location": "Slough, UK",
-            "status": "Not Applied",
-            "skills": ["HTML", "CSS", "Java"],
-            "schedule": "9AM-5PM Mon-Fri"
-        },
-        {
-            "title": "Fish Feeder",
-            "description": "Feed fish car batteries fish at the wall fish very very well",
-            "type": "Agriculture",
-            "location": "Missusaga, ON",
-            "status": "Rejected",
-            "skills": ["HTML", "CSS", "Java"],
-            "schedule": "9AM-5PM Mon-Fri"
+    const [jobs, setJobs] = useState([])
+
+    useEffect(() => {
+        async function fetchJobs() {
+            const { data: job_postings, error } = await supabase
+                .from('job_posts')
+                .select(`
+                        *,
+                        companies!job_posts_company_id_fkey (
+                        company_name
+                        )
+                    `)
+
+            setJobs(job_postings || [])
         }
-    ]
+
+
+        fetchJobs()
+    }, [])
+    console.log(jobs)
 
     return (
         <div className="sjb-page-container">
@@ -39,29 +34,29 @@ export default function Home() {
             {/* List of Job Items */}
             <div className="sjb-job-list-container">
                 {
-                    dummyData.map((item, idx) => (
+                    jobs.map((item, idx) => (
                         <div className="sjb-job-item-container" key={idx}>
                             <div className="sbj-job-item-top-container">
                                 <div className="sbj-job-item-top-left">
                                     <p className="sbj-job-item-title-text">{item.title}</p>
                                     <p className="sbj-job-item-company-text">
-                                        Company Name Here
-                                        <span className="sbj-job-item-info-text">Type: {item.type} | Location: {item.location} | Status: {item.status}</span>
+                                        {item.companies?.company_name}
+                                        <span className="sbj-job-item-info-text">Type: {item?.job_type} | Location: {item?.location} | Status: {item?.status}</span>
                                     </p>
                                 </div>
                                 <div className="sbj-job-item-top-right">
-                                    <button className="button m-5 text-3xl">Apply</button>
+                                    <button className="button m-5 text-2xl">Apply</button>
                                 </div>
                             </div>
                             <div className="sbj-job-item-bottom-container">
                                 <div className="sbj-job-item-bottom-about-container">
                                     <p className="font-bold">About: </p>
-                                    <p>{item.description}</p>
+                                    <p>{item?.description}</p>
                                 </div>
 
                                 <div className="sbj-job-item-bottom-skills-container"><p className="font-bold">Required Skills:</p>
                                     <ul className="sbj-job-item-skills-list">
-                                        {item.skills.map((item, idx) => (
+                                        {item?.required_skills?.map((item, idx) => (
                                             <li className="sbj-job-item-skills-list-item" key={idx}>{item}</li>
                                         ))}
                                     </ul>
@@ -69,7 +64,13 @@ export default function Home() {
 
                                 <div className="sbj-job-item-bottom-schedule-container">
                                     <p className="font-bold">Schedule:</p>
-                                    <p>{item.schedule}</p>
+                                    <div>
+                                        {item?.schedule?.split(" | ").map((item, idx) => (
+                                            <div key={idx}>
+                                                <p>{idx == 0 ? "Hours: " : "Days: "}{item}</p>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         </div>
