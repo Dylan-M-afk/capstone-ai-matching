7f457f016ec07b3daec5d19b3981b15cd861bd07
UnusualFrog <forwardnoah@gmail.com>
2026-06-21 14:31:34 -0230

    feat: Updated UI of company postings page

--- capstone-matching/app/company/dashboard/page.js
+++ capstone-matching/app/company/dashboard/page.js
@@ -3,23 +3,30 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="login-register-container">
-      <div className="login-register-btn-row">
+    <div className="sd-container">
+      <p className="page-header">Student Dashboard</p>
+      <div className="sd-content-container drop-shadow-2xl">
+        <p className="sd-body-text">
+          Welcome to the Company Dashboard. From here you can access the Current Posting page to view 
+          applicants to existing job postings. Check the Create Posting page to create new job postings for student applicants.
+        </p>
 
-        <Link href="postings">
-          <button className="button">
-            Current Postings
-          </button>
-        </Link>
-        <Link href="create-posting">
-          <button className="button">
-            Create Posting
-          </button>
-        </Link>
-
-
+        <div className='sd-btn-row'>
+          <Link href="postings">
+            <button className="button">
+              Current Postings
+            </button>
+          </Link>
+          <Link href="create-posting">
+            <button className="button">
+              Create Posting
+            </button>
+          </Link>
+        </div>
 
       </div>
+
     </div>
   );
 }
+
