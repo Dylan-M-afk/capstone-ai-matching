52837e8ec10c1993ed5eed9615a66c68c76a9c40
UnusualFrog <forwardnoah@gmail.com>
2026-06-21 12:41:01 -0230

    feat: New UI for home page and navbar complete

--- capstone-matching/app/page.js
+++ capstone-matching/app/page.js
@@ -3,14 +3,14 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="home-content-container">
+      <div className="home-content-container drop-shadow-2xl">
         <p className="home-header-text">READY TO CONNECT?</p>
         <p className="home-body-text">Here at [COMPANY NAME], we strive to
            improve the job market by connecting
           companies looking to hire with talented students using the power of LLMs to match applicants to the roles
           best suited to their abilities.</p>
           <Link href="/login">
-            <button className="home-login-btn">Login/Register</button>  
+            <button className="home-login-btn drop-shadow-lg">Login/Register</button>  
           </Link>
 
       </div>
